7f02809084ab0ab7adc1d2dbbc529550a090919c
Brian W Bush <brian.bush@nrel.gov>
2016-11-10 15:05:10 -0700

    tool for unmangling Excel files

---
+++ src/MainUnmangle.hs
@@ -0,0 +1,35 @@
+module Main (
+  main
+) where
+
+
+import Control.Arrow ((***), first, second)
+import Control.Monad (when)
+import System.Environment (getArgs)
+
+import qualified Data.ByteString.Char8 as BS
+
+main :: IO ()
+main =
+  do
+    args <- getArgs
+    case args of
+      []    -> putStrLn "Usage: unmangle-excel files..."
+      paths -> mapM_ unmangle paths
+
+
+unmangle :: FilePath -> IO ()
+unmangle path =
+  do
+    (modified, xs) <- unmangle' <$> BS.readFile path
+    print $ BS.length xs
+    when modified
+      $ BS.writeFile path xs
+
+
+unmangle' :: BS.ByteString -> (Bool, BS.ByteString)
+unmangle' xs
+  | BS.null xs         = (False, BS.empty)
+  | BS.head xs == '"'  = first (const True)            . unmangle' $ BS.tail xs
+  | BS.head xs == '\r' = const True *** BS.cons '\n'   $ unmangle' $ BS.tail xs
+  | otherwise          = second (BS.cons $ BS.head xs) . unmangle' $ BS.tail xs
