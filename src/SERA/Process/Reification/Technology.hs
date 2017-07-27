9c21611f1ef6b273485be6cd69ec2a73c769b41f
Brian W Bush <brian.bush@nrel.gov>
2017-07-27 12:42:44 -0600

    SERA-61 fixed missing price multiplier in flow table

--- src/SERA/Process/Reification/Technology.hs
+++ src/SERA/Process/Reification/Technology.hs
@@ -106,7 +106,7 @@ technologyReifier ProcessLibrary{..} intensityCube pricer specifics built capaci
               <+> fMaterial       =: upstream
               <+> fImpactCategory =: Upstream
               <+> fQuantity       =: quantity * intensity
-              <+> fSale           =: quantity * intensity -- * pricer upstream year
+              <+> fSale           =: quantity * intensity * pricer upstream year
             |
               rec <- toKnownRecords inputs
             , let material = fMaterial <: rec
