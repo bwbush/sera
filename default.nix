3a9cf43bcd149fb6857f2663428c642d34428b06
Brian W Bush <brian.bush@nrel.gov>
2018-02-27 10:22:52 -0700

    SERA-74 single-table checks for network

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 }:
 mkDerivation {
   pname = "sera";
-  version = "3.1.1.9";
+  version = "3.1.1.10";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
--- src/SERA/Network/IO.hs
+++ src/SERA/Network/IO.hs
@@ -37,7 +37,6 @@ import Control.Monad.Except (MonadError, MonadIO)
 import Control.Monad.Log (logDebug, logInfo, logError, logWarning)
 import Data.Aeson.Types (FromJSON, ToJSON)
 import Data.Daft.Vinyl.FieldCube (fromRecords)
-import Data.Daft.Vinyl.FieldCube.IO (readFieldCubeFile)
 import Data.Daft.Vinyl.FieldRec ((<:))
 import Data.Daft.Vinyl.FieldRec.IO (readFieldRecFile)
 import Data.Function.MapReduce (groupReduceByKey)
@@ -47,7 +46,8 @@ import Data.String (IsString)
 import GHC.Generics (Generic)
 import SERA (SeraLog)
 import SERA.Network.Algorithms
-import SERA.Network.Types (ExistingCube, LinkCube, fFrom, FLocation, fLocation, Network(..), NodeCube, TerritoryCube, fTo, ZoneCube)
+import SERA.Network.Types (ExistingCube, LinkCube, fFrom, FLocation, fLocation, Network(..), NodeCube, TerritoryCube, fTerritory, fTo, ZoneCube, fZone)
+import SERA.Types (fFraction)
 
 
 data NetworkFiles =
@@ -92,6 +92,7 @@ readConcat message files =
     ]
 
 
+checkDuplicates :: (SeraLog m, Ord b, Show b) => (String -> m ()) -> String -> (a -> b) -> [a] -> m ()
 checkDuplicates logMessage label field records =
   sequence_
     [
@@ -126,15 +127,47 @@ readLinks files =
 readExistings :: (IsString e, MonadError e m, MonadIO m, SeraLog m) => [FilePath] ->  m ExistingCube
 readExistings files =
   do
-    records <- readConcat "existings" files
+    records <- readConcat "existing facilitiess" files
     checkDuplicates logError "existing infrastructure" (fLocation <:) records
     checkDuplicates logDebug "existing endpoint" (fLocation <:) records
     return $ fromRecords records
 
 
+epsilon :: Double
+epsilon = 1e-5
+
+
+checkFractions label field records =
+  sequence_
+    [
+      logError $ "Fractions do not sum to one for " ++ label ++ " \"" ++ show location ++ "\"."
+    |
+      location <-
+        catMaybes
+          $ groupReduceByKey
+            field
+            (flip ((>>) . guard . (>= epsilon) . abs . ((-) 1) . sum . fmap (fFraction <:)) . return)
+            records
+    ]
+
+
 readTerritories :: (IsString e, MonadError e m, MonadIO m, SeraLog m) => [FilePath] ->  m TerritoryCube
-readTerritories = (mconcat <$>) . mapM readFieldCubeFile
+readTerritories files =
+  do
+    records <- readConcat "territories" files
+    let
+      key rec = (fTerritory <: rec, fLocation <: rec)
+    checkDuplicates logError "territory location" key records
+    checkFractions "territories" key records
+    return $ fromRecords records
 
 
 readZones :: (IsString e, MonadError e m, MonadIO m, SeraLog m) => [FilePath] ->  m (ZoneCube '[FLocation])
-readZones = (mconcat <$>) . mapM readFieldCubeFile
+readZones files =
+  do
+    records <- readConcat "zones" files
+    let
+      key rec = (fZone <: rec, fLocation <: rec)
+    checkDuplicates logError "zone location" key records
+    checkFractions "zones" key records
+    return $ fromRecords records
