59424abdb7e6308f352f96a4ffe8b02bfa462951
Brian W Bush <brian.bush@nrel.gov>
2018-03-01 07:38:09 -0700

    fix git urls

--- graft.nix
+++ graft.nix
@@ -3,7 +3,7 @@ mkDerivation {
   pname = "graft";
   version = "0.1.2.0";
   src = fetchgit {
-    url = "git://localhost/";
+    url = "git://10.40.9.156/";
     rev = "8eeeeead6fb734cf2b43f77e4245d85fb83193bf";
     sha256 = "181c1yml0zl7qr6wb6fq6lihj3bhkwh02bl5xjfdy0jcp1wa2mlw";
   };
